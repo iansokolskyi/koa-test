22175fa7ca256f437e78374108eae2115f757627
Gemofort <vanya6677@gmail.com>
2021-04-09 12:16:24 +0300

    Replaced drop constraint in down migration for email

--- src/migrations/down/3.UserPasswordAndLogin.sql
+++ src/migrations/down/3.UserPasswordAndLogin.sql
@@ -1,5 +1,5 @@
 START TRANSACTION;
   ALTER TABLE "user" DROP COLUMN "password";
-  ALTER TABLE "user" DROP COLUMN email;
   ALTER TABLE "user" DROP CONSTRAINT user_email;
+  ALTER TABLE "user" DROP COLUMN email;
 COMMIT;
